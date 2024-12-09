9096bc67e3f8dccb745b7019df3b726de0d17766
Vadim Zubchenko <vadimzubchenko@hotmail.com>
2024-12-10 00:23:15 +0200

    Added test sum of likes of blogs

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  // need that each element of array passes through function.
+  var initialValue = 0
+  const reducer = (previousBlog, currentBlog) => {
+    return previousBlog + currentBlog.likes
+  }
+  return blogs.length === 0 ? 0 : blogs.reduce(reducer, initialValue)
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
