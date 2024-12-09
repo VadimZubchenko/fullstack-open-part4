94044e5c2b11cf1ff4b17036a96e214fb5ca850c
Vadim Zubchenko <vadimzubchenko@hotmail.com>
2024-12-09 19:18:01 +0200

    Add blogs array tests

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
+// Create server as an app
 const express = require('express')
 const app = express()
+
+// Create routers
 const blogsRouter = require('./controllers/blogs')
-const middleware = require('./utils/middleware')
+
+// Use middleware for logging
+const requestLogger = require('./utils/middleware').requestLogger
+
+// Take parameters and mongoose for MongoDB connection
 const config = require('./utils/config')
 const mongoose = require('mongoose')
+
+// Print info on consol
 const { info, error } = require('./utils/logger')
 
+// mongoDB url
 const mongoUrl = config.MONGODB_URI
 
 // false allows queries to use fields that are not defined in the schema
 mongoose.set('strictQuery', false)
-info('Connecting to ', mongoUrl)
+
+// Connect to mongoDB
+info('Connecting to', mongoUrl)
 
 mongoose
   .connect(mongoUrl)
@@ -22,7 +34,7 @@ mongoose
   })
 
 app.use(express.json())
-app.use(middleware.requestLogger)
+app.use(requestLogger)
 app.use('/api/blogs', blogsRouter)
 
 module.exports = app
