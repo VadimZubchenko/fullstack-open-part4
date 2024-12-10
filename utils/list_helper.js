91ffe94fea683f390a9bad4fa6f1a95d58d9e53a
Vadim Zubchenko <vadimzubchenko@hotmail.com>
2024-12-10 14:36:09 +0200

    Add test of find a blog with max likes

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -10,8 +10,15 @@ const totalLikes = (blogs) => {
   }
   return blogs.length === 0 ? 0 : blogs.reduce(reducer, initialValue)
 }
+const maxLikes = (blogs) => {
+  const likes = blogs.map((blog) => blog.likes)
+  const max = Math.max(...likes)
+
+  return blogs.find((blog) => blog.likes === max)
+}
 
 module.exports = {
   dummy,
   totalLikes,
+  maxLikes,
 }
